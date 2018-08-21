78e4477f1fa56b6918bd4616bf224087bcfa3075
Ermeson <ermeson16000@gmail.com>
2018-08-21 14:22:49 -0300

    Consertando adição de

--- projeto_adalt/projetos/views.py
+++ projeto_adalt/projetos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from projetos.forms import ProjetoForm
+from .models import *
+from django.shortcuts import redirect
 
 # Create your views here.
 @login_required
@@ -9,8 +11,12 @@ def novo_projeto(request):
     form = ProjetoForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
-            projeto = form.save()
-            login(request, usuario)
+            projeto = Projeto()
+            projeto.nome = request.POST.get('nome', '')
+            projeto.setor = request.POST.get('setor', '')
+            projeto.descricao = request.POST.get('descricao', '')
+            projeto.usuario = request.user
+            projeto.save()
             return redirect('index')
     context = {
         'form': form
